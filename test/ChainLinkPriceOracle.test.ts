b007763e316912ce5119be7470c0912a7f7f562e
defioctopus <defioctopus@gmail.com>
2021-09-08 00:07:09 -0300

    add owner and setFeed

--- test/ChainLinkPriceOracle.test.ts
+++ test/ChainLinkPriceOracle.test.ts
@@ -1,15 +1,20 @@
 import { expect } from 'chai'
 import { Contract } from 'ethers'
-import { fp, deploy, bn, ZERO_ADDRESS } from '@mimic-fi/v1-helpers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { assertEvent, fp, deploy, bn, ZERO_ADDRESS, getSigners } from '@mimic-fi/v1-helpers'
 
 describe('ChainLinkPriceOracle', () => {
   let oracle: Contract
   let tokenA: Contract, tokenB: Contract, tokenC: Contract, tokenD: Contract
   let feedTokenA: Contract, feedTokenC: Contract, feedTokenD: Contract
+  let admin: SignerWithAddress, other: SignerWithAddress
 
   const PRICE_ONE_FEED = '0x1111111111111111111111111111111111111111'
 
   beforeEach('create oracle', async () => {
+    // eslint-disable-next-line prettier/prettier
+    ;[admin, other] = await getSigners()
+
     tokenA = await deploy('TokenMock', ['TokenA', 18])
     tokenB = await deploy('TokenMock', ['TokenB', 18])
     tokenC = await deploy('TokenMock', ['TokenC', 6])
@@ -34,19 +39,19 @@ describe('ChainLinkPriceOracle', () => {
       expect(await oracle.hasPriceFeed(tokenD.address)).to.be.true
 
       const feedA = await oracle.getPriceFeed(tokenA.address)
-      expect(feedA.aggregator).to.be.equal(feedTokenA.address)
+      expect(feedA.feed).to.be.equal(feedTokenA.address)
       expect(feedA.tokenDecimals).to.be.equal(await tokenA.decimals())
 
       const feedB = await oracle.getPriceFeed(tokenB.address)
-      expect(feedB.aggregator).to.be.equal(PRICE_ONE_FEED)
+      expect(feedB.feed).to.be.equal(PRICE_ONE_FEED)
       expect(feedB.tokenDecimals).to.be.equal(await tokenB.decimals())
 
       const feedC = await oracle.getPriceFeed(tokenC.address)
-      expect(feedC.aggregator).to.be.equal(feedTokenC.address)
+      expect(feedC.feed).to.be.equal(feedTokenC.address)
       expect(feedC.tokenDecimals).to.be.equal(await tokenC.decimals())
 
       const feedD = await oracle.getPriceFeed(tokenD.address)
-      expect(feedD.aggregator).to.be.equal(feedTokenD.address)
+      expect(feedD.feed).to.be.equal(feedTokenD.address)
       expect(feedD.tokenDecimals).to.be.equal(await tokenD.decimals())
     })
 
@@ -140,6 +145,81 @@ describe('ChainLinkPriceOracle', () => {
       const scaledTokenCalcPrice = calcPrice.mul(oneInTokenDecimals).div(fp(1))
 
       expect(unscaledBasePrice).to.be.equal(scaledTokenCalcPrice)
+    })
+  })
+
+  describe('set price feed', () => {
+    let from: SignerWithAddress
+
+    context('when the sender is the admin', () => {
+      beforeEach('set sender', async () => {
+        from = admin
+      })
+
+      context('when the feed is not set', () => {
+        it('updates the price oracle', async () => {
+          const newToken = await deploy('TokenMock', ['TokenE', 18])
+          const newFeed = await deploy('ChainLinkAggregatorV3', [18])
+
+          const feedBefore = await oracle.getPriceFeed(newToken.address)
+          expect(feedBefore.feed).to.be.equal(ZERO_ADDRESS)
+
+          await oracle.connect(from).setPriceFeed(newToken.address, newFeed.address)
+
+          const feedAfter = await oracle.getPriceFeed(newToken.address)
+          expect(feedAfter.feed).to.be.equal(newFeed.address)
+          expect(feedAfter.tokenDecimals).to.be.equal(await newToken.decimals())
+        })
+
+        it('reverts if feed decimals is not 18', async () => {
+          const newFeed = await deploy('ChainLinkAggregatorV3', [10])
+          await expect(oracle.connect(from).setPriceFeed(tokenA.address, newFeed.address)).to.be.revertedWith('INVALID_FEED_DECIMALS')
+        })
+      })
+
+      context('when the feed is already set', () => {
+        it('updates the price oracle', async () => {
+          const feedBefore = await oracle.getPriceFeed(tokenA.address)
+          expect(feedBefore.feed).to.be.equal(feedTokenA.address)
+          expect(feedBefore.tokenDecimals).to.be.equal(await tokenA.decimals())
+
+          const newFeed = await deploy('ChainLinkAggregatorV3', [18])
+
+          await oracle.connect(from).setPriceFeed(tokenA.address, newFeed.address)
+
+          const feedAfter = await oracle.getPriceFeed(tokenA.address)
+          expect(feedAfter.feed).to.be.equal(newFeed.address)
+          expect(feedAfter.tokenDecimals).to.be.equal(await tokenA.decimals())
+        })
+
+        it('emits an event', async () => {
+          const newFeed = await deploy('ChainLinkAggregatorV3', [18])
+
+          const tx = await oracle.connect(from).setPriceFeed(tokenA.address, newFeed.address)
+          await assertEvent(tx, 'PriceFeedSet', { token: tokenA.address, feed: newFeed.address })
+        })
+
+        it('unsets the price oracle', async () => {
+          await oracle.connect(from).setPriceFeed(tokenA.address, ZERO_ADDRESS)
+
+          const feedAfter = await oracle.getPriceFeed(tokenA.address)
+          expect(feedAfter.feed).to.be.equal(ZERO_ADDRESS)
+        })
+
+        it('reverts', async () => {
+          await expect(oracle.connect(from).setPriceFeed(tokenA.address, feedTokenA.address)).to.be.revertedWith('FEED_ALREADY_SET')
+        })
+      })
+    })
+
+    context('when the sender is not the admin', () => {
+      beforeEach('set sender', async () => {
+        from = other
+      })
+
+      it('reverts', async () => {
+        await expect(oracle.connect(from).setPriceFeed(tokenA.address, feedTokenA.address)).to.be.revertedWith('Ownable: caller is not the owner')
+      })
     })
   })
 })
