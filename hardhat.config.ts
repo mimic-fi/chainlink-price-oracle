96bed40b8ebb60aa7d636b046c5f007a8d56dc9e
lkndjrt <lkndjrt@gmail.com>
2022-06-10 09:59:16 -0300

    chore: install local hardhat config plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,13 @@
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@mimic-fi/v1-helpers/dist/tests'
+import 'hardhat-local-networks-config-plugin'
+
+import { homedir } from 'os'
+import path from 'path'
 
 export default {
+  localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic.json'),
   solidity: {
     version: '0.8.0',
     settings: {
